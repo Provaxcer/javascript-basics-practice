0eed526fad5f55a18d8a9b5369ef7845c9f29aac
Provaxcer <basaksantanu659@gmail.com>
2026-05-07 18:50:58 +0530

    class

--- Fullstack/js 4/1.js
+++ Fullstack/js 4/1.js
@@ -5,14 +5,16 @@ class Book {
         this.author = author;
     }
 
-    publish(country, region) {
+    static publish(country, region) {
         this.country = country;
         this.region = region;
+
+        return this;
     }
 }
 
 const book1 = new Book ('Ikigai', '2022', 'John')
-const book2 = new Book.publish ('Japan', 'Saitama')
+const book2 = Book.publish ('Japan', 'Saitama')
 
 console.log(book1.author);
-console.log(book2);
+console.log(book2.country);
